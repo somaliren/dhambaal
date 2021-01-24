9ef8fa08af61ad1ea6dc3bfdf65dcbaeb490bd15
Amin Abdisamad <amin.abdisamad@b4finance.com>
2021-01-25 01:20:25 +0300

    added

---
+++ generator_1.py
@@ -0,0 +1,17 @@
+# Waxaan abuuraynaa function noo sheegaya in tirada ay tahay dhaban ama kisi
+
+def ma_dhabanbaa(lambar):
+    if lambar % 2 == 0:
+        return True
+    False
+    
+
+
+for i in range(100):
+    if ma_dhabanbaa(i):
+        print(i)
+        
+    
+    
+
+
--- project_homework.py
+++ project_homework.py
@@ -26,3 +26,103 @@
 # Also  print benefits and projects they will work on
 # otherwise, prompt the user that they haven't fulfilled the requirements [Sorry Our job requires x years of experience]
 # name = input("whats your name")
+
+
+
+from dataclasses import dataclass
+from typing import List,Optional
+
+# @dataclass
+# class OpeningPositions:
+#     name:str
+#     location:str
+#     years_of_exp:int
+#     shift:str =  None
+
+#     def __repr__(self):
+#         return f"{self.name} in {self.location} requires {self.years_of_exp} years of experience"
+
+def opining_positions(*,name,location,years_of_exp,shift=None):
+    return {"name":name,"location":location,'years_of_exp':years_of_exp}
+    # return f"{name} in {location} requires {years_of_exp} years of experience" 
+
+@dataclass
+class Benifits:
+    name:str
+
+    def __repr__(self):
+        return f"{self.name}"
+
+
+# Opening 
+
+OPENING_POSITIONS = [
+    opining_positions(name="System Engineering",location="Mogadishu",years_of_exp=3,shift="Night"),
+    opining_positions(name="Network Engineering",location="Mogadishu",years_of_exp=5)
+# OpeningPositions(name="System Engineering",location="Mogadishu",years_of_exp=3,shift="Night"),
+# OpeningPositions(location="Mogadishu",name="Network Engineering",years_of_exp=5),
+# OpeningPositions(location="Nairobi",name="DevOps",years_of_exp=8),
+]
+
+position1=  opining_positions(name="System Engineering",location="Mogadishu",years_of_exp=3,shift="Night")
+print(position1["name"])
+
+# print(OPENING_POSITIONS[0].name)
+# print(OPENING_POSITIONS[1].name)
+
+
+BENEFITS = [
+    Benifits("Free lunch"),
+    Benifits("Remote Work"),
+]
+
+@dataclass
+class Company:
+    benefits:List[Benifits]
+
+    def invite(self):
+        for benefit in self.benefits:
+            print(benefit)
+
+
+somaliren = Company(benefits=BENEFITS)
+somaliren.invite()
+
+
+
+MAX = 14 # user enters 4
+
+# Looping Opening positions 
+for index,job in enumerate(OPENING_POSITIONS):
+    if OPENING_POSITIONS[index]["years_of_exp"] <= MAX:
+        print(f"{index+1}. {job['name']} in {job['location']} requires {job['years_of_exp']} years of experience")
+
+# option = int(input("Select Option: "))
+# option -=1
+# print(option)
+
+# loves_networking = True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# OPENING_POSITIONS_DICT = [
+# {"positionName":"Netowrk"},
+# {"positionName":"System"},
+# ]
+# # dictname.get("value")
+# print(OPENING_POSITIONS_DICT[0].get("positionName"))
+# print(OPENING_POSITIONS_DICT[1].get("positionName"))
+
+
+
